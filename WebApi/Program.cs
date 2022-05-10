55eb35609eeda886c2492ae0caf0a3a826d2565e
samic <sami.c@hotmail.be>
2022-05-10 21:27:57 +0200

    enable swagger for azure

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -57,12 +57,8 @@ builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseCors();
 app.UseAuthentication();
