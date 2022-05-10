0c217bbcc3c6b5dda27204c416e185137fc43bbe
samic <sami.c@hotmail.be>
2022-05-10 21:59:24 +0200

    ErrorController added

---
+++ WebApi/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ErrorController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult HandleError() =>
+        Problem();
+}
