37f11f25b168e5236951618a80536a9c1ff6bb24
samic <sami.c@hotmail.be>
2022-05-10 22:02:15 +0200

    AllowAnyOrigin

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000")
+            builder.AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
